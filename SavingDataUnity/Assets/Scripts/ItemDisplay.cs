8251f0fa667601cbb796c3a06d11241d3464c51c
Batora07 <batora.ushiromiya.07@gmail.com>
2019-06-02 16:49:42 +0200

    Add working XML Parser for database purpose

---
+++ SavingDataUnity/Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDisplay : MonoBehaviour
+{
+	public ItemBlock blockPrefab;
+
+	private void Start()
+	{
+		Display();
+	}
+
+	public void Display()
+	{
+		foreach(Transform child in transform)
+		{
+			Destroy(child.gameObject);
+		}
+
+		foreach(ItemEntry item in XMLManager.instance.itemDB.list)
+		{
+			ItemBlock newBlock = Instantiate(blockPrefab) as ItemBlock;
+			newBlock.transform.SetParent(transform, false);
+
+			// set ui of the item
+			newBlock.Display(item);
+		}
+	}
+}
---
+++ SavingDataUnity/Assets/Scripts/XMLManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;	// lets us use lists
+using UnityEngine;
+using System.Xml;					// basic XML attributes
+using System.Xml.Serialization;		// access xmlSerialiser
+using System.IO;					// file management
+
+public class XMLManager : MonoBehaviour
+{
+	public static XMLManager instance;
+
+	private void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	/// <summary>
+	/// lists of items
+	/// </summary>
+	public ItemDatabase itemDB;
+
+	// save function
+	public void SaveItems()
+	{
+		// open a new xml file
+		XmlSerializer serializer = new XmlSerializer(typeof(ItemDatabase));
+		/**
+		 * classical way of serializing streams
+		 * */
+		 /* FileStream stream = new FileStream(Application.dataPath + "/StreamingAssets/Data/XML/item_data.xml", FileMode.Create);
+			serializer.Serialize(stream, itemDB); */
+
+		// this method prevents errors with encoding streams
+		var encoding = System.Text.Encoding.GetEncoding("UTF-8");
+		using(var stream = new StreamWriter(Application.dataPath + "/StreamingAssets/Data/XML/item_data.xml", false, encoding))
+		{
+			serializer.Serialize(stream, itemDB);
+			stream.Close();
+		}
+		// stream.Close();
+	}
+
+	// load function
+	public void LoadItems()
+	{
+		XmlSerializer serializer = new XmlSerializer(typeof(ItemDatabase));
+		FileStream stream = new FileStream(Application.dataPath + "/StreamingAssets/Data/XML/item_data.xml", FileMode.Open);
+		itemDB = (ItemDatabase)serializer.Deserialize(stream);
+		stream.Close();
+	}
+}
+
+[System.Serializable]
+public class ItemEntry
+{
+	public string itemName;
+	public Material material;
+	public int value;
+}
+
+[System.Serializable]
+public class ItemDatabase
+{
+	[XmlArray("CombatEquipment")]
+	public List<ItemEntry> list = new List<ItemEntry>();
+}
+
+public enum Material
+{
+	Wood,
+	Copper,
+	Iron,
+	Steel,
+	Obsidian
+}
